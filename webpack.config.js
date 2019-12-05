110e43d5560d5dc5ee8ad5ef1ca50a20ba039b5d
Patryk <piskorz.patryk.96@gmail.com>
2019-12-05 16:07:10 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,6 @@ const PATHS = {
     src: path.resolve(__dirname, 'src')
 };
 
-function recursiveIssuer(m) {
-    if (m.issuer) {
-        return recursiveIssuer(m.issuer);
-    } else if (m.name) {
-        return m.name;
-    } else {
-        return false;
-    }
-}
-
 module.exports = {
     entry: {
         server: "./server/app.js",
