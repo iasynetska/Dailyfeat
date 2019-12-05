b4f687266dcf635ea6836f0325c0852c6649d8ca
Patryk <piskorz.patryk.96@gmail.com>
2019-12-05 15:07:40 +0100

    WebPack Config

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const glob = require("glob")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+const PATHS = {
+    src: path.resolve(__dirname, 'src')
+};
+
+function recursiveIssuer(m) {
+    if (m.issuer) {
+        return recursiveIssuer(m.issuer);
+    } else if (m.name) {
+        return m.name;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    entry: {
+        pageOne: "./src/pageOne/app.js",
+        pageTwo: "./src/pageTwo/app.js"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name]/bundle.js",
+    },
+    watch: false,
+    mode: "development",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            '@babel/plugin-proposal-object-rest-spread',
+                            "@babel/plugin-transform-runtime"
+                        ],
+                        comments: false
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/pageOne/index.html",
+            inject: true,
+            chunks: ['pageOne'],
+            filename: 'pageOne/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/pageTwo/index.html",
+            inject: true,
+            chunks: ['pageTwo'],
+            filename: 'pageTwo/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+        }),
+    ]
+}
