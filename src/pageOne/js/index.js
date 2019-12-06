aed1487f5307211c0a77424c931e0fdbebd3d250
Yura <yurik250501@mail.ru>
2019-12-06 14:09:57 +0100

    steps

--- src/pageOne/js/index.js
+++ src/pageOne/js/index.js
@@ -1,14 +1,14 @@
-// let next = document.querySelector('#nextButton');
-// let registration = document.querySelector('.registration');
-// let emailOptin = document.querySelector('.emailOptin'); 
+let next = document.querySelector('#nextButton');
+let registration = document.querySelector('.registration');
+let emailOptin = document.querySelector('.emailOptin'); 
 
+function nextStep() {
+    registration.classList.add('extinction');
+    emailOptin.style.display = 'flex';
+    registration.addEventListener('animationend', () => {
+        registration.style.display = 'none';
+        
+    })
+}
 
-// function nextStep() {
-//     registration.classList.add('extinction');
-//     registration.addEventListener('animationend', () => {
-//         registration.style.display = 'none';
-//         emailOptin.style.display = 'flex';
-//     })
-// }
-
-// next.addEventListener('click', nextStep);
+next.addEventListener('click', nextStep);
