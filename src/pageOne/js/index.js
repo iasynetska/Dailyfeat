64c5c17dca9f1116e6f8143342100b98f22ef937
Yura <yurik250501@mail.ru>
2019-12-06 14:27:10 +0100

    fix

--- src/pageOne/js/index.js
+++ src/pageOne/js/index.js
@@ -1,8 +1,16 @@
 let next = document.querySelector('#nextButton');
 let registration = document.querySelector('.registration');
+let emailInput = registration.querySelector('input[type=email]');
 let emailOptin = document.querySelector('.emailOptin'); 
 
 function nextStep() {
+    if (!emailInput.value) {
+        emailInput.style.boxShadow = '0px 0px 10px red';
+        emailInput.addEventListener('change', () => {
+            emailInput.style.boxShadow = '0px 0px 15px rgba(0, 0, 0, 0.329)';
+        });
+        return;
+    };
     registration.classList.add('extinction');
     emailOptin.style.display = 'flex';
     registration.addEventListener('animationend', () => {
