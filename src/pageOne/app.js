4eb87912143a45fff5141e91b6d873b5b1e71151
Yura <yurik250501@mail.ru>
2019-12-06 13:36:49 +0100

    icons

--- src/pageOne/app.js
+++ src/pageOne/app.js
@@ -3,7 +3,5 @@ import "./style.css";
 import "./emailOptin.css";
 
 import "./images/daily-feat-logo.svg";
-import "./images/google.svg";
-import "./images/facebook.svg";
 
-// import "./js/index.js";
+import "./js/index.js";
