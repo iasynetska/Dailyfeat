4718194c5f1d286b786ac0e5ed0552485e45cff8
Vika <viktoriia.iasynetska@gmail.com>
2019-12-08 11:53:28 +0100

    Installed lodash and bcrypt; added hashing password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const {User, validate} = require('../models/userModel');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -7,18 +9,15 @@ router.post('/', async (req, res) => {
 	const { error } = validate(req.body);
 	if(error) return res.status(400).send(error.details[0].message);
 
-	let user = await User.findOne({ email:req.body.email });
+	let user = await User.findOne( {$or: [ {login:req.body.login},  {email:req.body.email} ]});
 	if(user) return res.status(400).send('User already registered.');
 
-	user = new User({
-		login: req.body.login,
-		email: req.body.email,
-		password: req.body.password
-	});
-
+	user = new User(_.pick(req.body, ['login', 'email', 'password']));
+	const salt = await bcrypt.genSalt(10);
+	user.password = await bcrypt.hash(user.password, salt);
 	await user.save().catch(error => console.log(error));
 
-	res.send(user);
+	res.send(_.pick(user, ['_id', 'login', 'email']));
 });
 
 module.exports = router;
