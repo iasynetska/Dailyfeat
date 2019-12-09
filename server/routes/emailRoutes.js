2fdcd410a9772d3450943025c35317260728fe8b
Patryk <piskorz.patryk.96@gmail.com>
2019-12-09 16:53:39 +0100

    Email Checker

--- server/routes/emailRoutes.js
+++ server/routes/emailRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const {validateEmail} = require('../models/userModel');
+const {validateEmail, User} = require('../models/userModel');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     const { error } = validateEmail(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    res.status(500).send("OK");
+    const user = await User.findOne({email: req.body.email});
+    if(user) return res.status(200).send(true)
+
+    res.status(200).send(false);
 });
 
 module.exports = router;
